ac256a4762a554f461c12bab3654c1da396173c0
Adominguezes <alberto@adominguez.es>
2017-04-24 22:15:57 +0200

    initial commit

---
+++ adom-panel-menu.js
@@ -0,0 +1,24 @@
+(function () {
+  'use strict';
+  Polymer({
+
+    is: 'adom-panel-menu',
+
+    properties: {
+      menu: {
+        type: Array,
+        value: function() {
+          return [];
+        }
+      }
+    },
+    attached: function() {
+      for (var i = 0; i < this.menu.length; i++) {
+        if((this.menu[i].src) && (!this.menu[i].size)) {
+          this.menu[i].size = 200;
+        }
+      }
+    }
+
+  });
+}());
